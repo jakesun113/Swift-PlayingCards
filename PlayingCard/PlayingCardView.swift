41c041c74e5ee6d48a19199850c7fb2c0ef0aed0
wry <wry@wrydemacbook-pro.local>
2018-08-25 15:15:40 +1000

    make the corner of the screen round

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -14,33 +14,11 @@ class PlayingCardView: UIView {
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
-        //stroke but not fill
-//        if let context = UIGraphicsGetCurrentContext()
-//        {
-//            //draw a circle
-//            context.addArc(center: CGPoint(x:bounds.midX, y: bounds.midY),
-//                           radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-//            context.setLineWidth(5.0)
-//            UIColor.green.setFill()
-//            UIColor.red.setStroke()
-//            context.strokePath()
-//            context.fillPath()
-//        }
-        
-        
-        //stroke and fill
-        //but if rotate, it will not be a circle anymore
-        //to fix this, change "main story board" view -> content mode to "redraw"
-        let path = UIBezierPath()
-        path.addArc(withCenter: CGPoint(x:bounds.midX, y: bounds.midY),
-                       radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-        path.lineWidth = 5.0
-        UIColor.green.setFill()
-        UIColor.red.setStroke()
-        path.stroke()
-        path.fill()
-        
+        //make the corner of the screen round
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        roundedRect.addClip()
+        UIColor.white.setFill()
+        roundedRect.fill()
     }
  
 
